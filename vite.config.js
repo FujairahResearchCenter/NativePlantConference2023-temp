ca78ab1b71e77fe8b0ce63e8708320fc2e9a893d
Fujairah Research Center <108390870+FujairahResearchCenter@users.noreply.github.com>
2023-02-08 11:26:05 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   
-  server: {
-    port: 8080,
-  },
   plugins: [vue()],
   base: '/NativePlantConference2023/',
   resolve: {
